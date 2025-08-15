a72343067517a48c8c8ccf214d72d7e0c1885ad4
danixek <danielkefurt@gmail.com>
2025-08-15 14:53:06 +0200

    Project descriptions and notes was edited - again. Logging in PHP - better blocking IP adresses (blacklist)

--- logging.php
+++ logging.php
@@ -20,8 +20,15 @@ if (basename($_SERVER['PHP_SELF']) === "detail.php" && isset($_GET['id']) && htm
     $logLine = "[$date] - $userLocation - IP: $ip - UA: $ua" . PHP_EOL;
 }
 
-if ($ip !== ("2001:1ae9:15e:bc00:a437:bf78:53d5:d7eb"
-|| "2001:1ae9:15e:bc00:34b2:24c1:bda8:b9a9")) {
+$blockedIps = [
+    "2001:1ae9:15e:bc00:a437:bf78:53d5:d7eb",
+    "2001:1ae9:15e:bc00:34b2:24c1:bda8:b9a9",
+    "2001:1aef:183:46a7:0:4e:530f:c701",
+    "193.86.230.167",
+    "194.212.160.90"
+];
+
+if (!in_array($ip, $blockedIps, true)) {
 
     // přidání do souboru
     file_put_contents($logFile, $logLine, FILE_APPEND | LOCK_EX);
